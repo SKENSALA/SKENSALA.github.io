ee2ab899d4aacfe559230764b66a5dd6d486fafe
neovasi <novasi.team@gmail.com>
2021-07-13 06:57:25 +0700

    push arimbi

--- js/script.js
+++ js/script.js
@@ -30,3 +30,20 @@ $('.faq li .question').click(function () {
   $(this).find('.plus-minus-toggle').toggleClass('collapsed');
   $(this).parent().toggleClass('active');
 });
+
+
+// back to top
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
