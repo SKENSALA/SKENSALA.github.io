873d85ddee1111ca3d6c638e7cc376877e81d152
neovasi <novasi.team@gmail.com>
2021-07-12 17:24:40 +0700

    comit 1

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const menuToggle = document.querySelector(".menu-toggle input");
+const nav = document.querySelector("nav ul");
+
+menuToggle.addEventListener("click", function () {
+  nav.classList.toggle("slide");
+});
+
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
+
+window.addEventListener("scroll", function() {
+  var header = document.querySelector("nav");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+// const quest = document.querySelector(".quest");
+// const answer = document.querySelector(".answer")
+// const up = document.querySelector(".up")
+
+// quest.addEventListener("click" , function(e) {
+//   answer.classList.toggle("show");
+//   up.classList.toggle("rotate");
+// });
+
+$('.faq li .question').click(function () {
+  $(this).find('.plus-minus-toggle').toggleClass('collapsed');
+  $(this).parent().toggleClass('active');
+});
